35264e67b6403c06d74dd0b5eec2f1c74367e80f
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-03 15:46:05 +0300

    Если нажать отмена, то игра завершается

--- script.js
+++ script.js
@@ -1,20 +1,49 @@
-const n = 100;
+let NextOrStop = function () {
+  if (!confirm("Начать новую игру?")) {
+    alert("Игра завершена!");
+  } else {
+    guessTheNumber();
+  }
+};
 
-let NumberIsLessOrGreater = function (question) {
-  if (question < n) {
-    alert("Загаданное число больше!");
-  } else if (question > n) {
-    alert("Загаданное число меньше!");
+let isNumber = function (num) {
+  if (isNaN(question)) {
+    alert("Введите число!");
+    question = prompt("Угадай число от 1 до 100");
   }
 };
 
 const guessTheNumber = function () {
-  let question = +prompt("Угадай число от 1 до 100");
-  if (question !== n) {
-    NumberIsLessOrGreater(question);
-    guessTheNumber();
+  const n = 100;
+
+  let question = prompt("Угадай число от 1 до 100");
+
+  if (question === null) {
+    alert("Игра завершена!");
   } else {
-    alert("Поздравляю, вы угадали!");
+    if (isNaN(question)) {
+      alert("Введи число!");
+      question = prompt("Угадай число от 1 до 100");
+    }
+
+    let conversion = parseInt(question);
+    console.log(conversion);
+
+    let NumberIsLessOrGreater = function (num) {
+      if (num < n) {
+        alert("Загаданное число больше!");
+      } else if (num > n) {
+        alert("Загаданное число меньше!");
+      }
+    };
+
+    if (conversion !== n) {
+      NumberIsLessOrGreater(conversion);
+      guessTheNumber();
+    } else {
+      alert("Поздравляю, вы угадали!");
+      NextOrStop();
+    }
   }
 };
 
