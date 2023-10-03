ab5d65f10426175fc254b8a87a4429d551f9d76c
Nikita Dubinin <nik.dubinin.01@mail.ru>
2023-10-03 13:47:21 +0300

    Реализовал подсказки для пользователя (число больше или меньше)

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const n = 100;
 
+let NumberIsLessOrGreater = function (question) {
+  if (question < n) {
+    alert("Загаданное число больше!");
+  } else if (question > n) {
+    alert("Загаданное число меньше!");
+  }
+};
+
 const guessTheNumber = function () {
   let question = +prompt("Угадай число от 1 до 100");
   if (question !== n) {
+    NumberIsLessOrGreater(question);
     guessTheNumber();
   } else {
-    alert("Вы угадали число!");
+    alert("Поздравляю, вы угадали!");
   }
 };
 
